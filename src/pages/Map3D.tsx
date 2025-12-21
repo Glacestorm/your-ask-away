95f609e4c582d1dd2274b1ce63a688923580fade
Glacestorm <familia.c.aguilar@gmail.com>
2025-12-21 21:53:57 +0100

    Update Map3D.tsx

--- src/pages/Map3D.tsx
+++ src/pages/Map3D.tsx
@@ -1,12 +1,35 @@
-import React, { useEffect } from 'react';
-import Map3DBuildings from '@/components/map/Map3DBuildings';
+// Ejemplo de inyección dinámica del CSS del mapa en el componente Map3D
+import { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl'; // o la librería que uses
 
-const Map3D: React.FC = () => {
+export default function Map3D(props: any) {
   useEffect(() => {
-    document.title = 'Mapa 3D - ObelixIA';
+    const href = 'https://api.mapbox.com/mapbox-gl-js/v3.8.0/mapbox-gl.css';
+    let linkEl = document.querySelector(`link[href="${href}"]`) as HTMLLinkElement | null;
+    let created = false;
+    if (!linkEl) {
+      linkEl = document.createElement('link');
+      linkEl.rel = 'stylesheet';
+      linkEl.href = href;
+      linkEl.crossOrigin = '';
+      document.head.appendChild(linkEl);
+      created = true;
+    }
+    // Inicializa mapa aquí (asegúrate de hacerlo cuando la CSS esté disponible si es necesario)
+    // const map = new mapboxgl.Map({...});
+
+    return () => {
+      // opcional: eliminar el link cuando se desmonta si lo creamos nosotros
+      if (created && linkEl && linkEl.parentNode) {
+        linkEl.parentNode.removeChild(linkEl);
+      }
+      // map?.remove();
+    };
   }, []);
 
-  return <Map3DBuildings />;
-};
-
-export default Map3D;
+  return (
+    <div id="map-root" style={{ width: '100%', height: '100%' }}>
+      {/* Placeholder hasta que el mapa se inicialice */}
+    </div>
+  );
+}
